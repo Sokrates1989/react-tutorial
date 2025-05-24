0bdc8fd8a924fa4bc9dd42633f8b7d5ce1a324c6
Patrick Michiels <it@patrickmichiels.de>
2025-05-24 20:32:06 +0200

    [New Feature]Template: Added Contact card component.

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+
+// Header, footer, note.
 import Note from "./components/Note";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+// Contact card.
+import ContactCard from "./components/ContactCard";
+import contacts from "./data/contacts";
 
 function App() {
   return (
@@ -10,6 +16,19 @@ function App() {
       <Note 
         title="Property Given Title" 
         content="Property based content" 
+      />
+      
+      <ContactCard
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <ContactCard
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
       />
       <Footer />
     </>
